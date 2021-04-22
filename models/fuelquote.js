736d87b54d8581bae54e1734cc0914a2309791db
aqazi328 <70555455+aqazi328@users.noreply.github.com>
2021-04-22 17:02:31 -0500

    Update fuelquote.js

--- models/fuelquote.js
+++ models/fuelquote.js
@@ -9,6 +9,36 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    constructor(_location, _history, _amount){
+      if (_location == true) {       // if in Texas
+          this.location = .02;
+      } else{
+          this.location = .04;
+      }
+      
+      if (_history == true) {       // if they are a prev customer  
+          this.history = .01;
+      } else {
+          this.history = 0;
+      }
+
+      this.galAmount = _amount
+      if (_amount > 1000) {       // if they are a prev customer  
+          this.amount = .02;
+      } else {
+          this.amount = .03;
+      }
+
+      this.profit_factor = .1;
+    }
+    getQuoteperGal(){
+        var margin = 1.5 * (this.location - this.history + this.amount + this.profit_factor);
+        return 1.5 + margin
+    }
+    getTotalQuote(){
+        var margin = 1.5 * (this.location - this.history + this.amount + this.profit_factor);
+        return this.galAmount * (1.5 + margin)
+    }
     
   };
   FuelQuote.init({
